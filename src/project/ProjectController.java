7377a915a6a25483d4c071fa614e6512a6e75bc9
YoonGangHoon <yghun021007@naver.com>
2025-11-10 13:04:45 +0900

    feat: 프로젝트 조회 후 엔터키를 입력해야 메뉴로 돌아갈 수 있는 기능 추가

--- src/project/ProjectController.java
+++ src/project/ProjectController.java
@@ -19,6 +19,8 @@ public class ProjectController {
                 System.out.println("1. 프로젝트 목록 보기");
                 System.out.println("2. 프로젝트 생성");
                 System.out.println("3. 내가 참여 중인 프로젝트 보기");
+                System.out.println("4. 프로젝트 수정");
+                System.out.println("5. 프로젝트 삭제");
                 System.out.println("b. 뒤로 가기");
             }
             else {
@@ -36,12 +38,16 @@ public class ProjectController {
                         int cnt = scanner.nextInt();
                         scanner.nextLine();
                         projectService.showProjectList(cnt);
+                        System.out.print("\n엔터키를 누르면 프로젝트 기능으로 돌아갑니다.");
+                        scanner.nextLine();
                         break;
                     case "2":
                         projectService.createProject(memberService.getCurrentUser());
                         break;
                     case "3":
                         projectService.showMyProjectList(memberService.getCurrentUser());
+                        System.out.print("\n엔터키를 누르면 프로젝트 기능으로 돌아갑니다.");
+                        scanner.nextLine();
                         break;
                     case "b":
                         return;
@@ -56,6 +62,8 @@ public class ProjectController {
                         int cnt = scanner.nextInt();
                         scanner.nextLine();
                         projectService.showProjectList(cnt);
+                        System.out.print("\n엔터키를 누르면 프로젝트 기능으로 돌아갑니다.");
+                        scanner.nextLine();
                         break;
                     case "b":
                         return;
