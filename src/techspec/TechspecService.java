cc18894cfcfc5d9058565318e1d6f9b17d42b062
chaechaekim <chaeeun7478@naver.com>
2025-11-16 16:06:54 +0900

    fix(techspec): Service 메서드가 Controller의 파라미터를 받도록 수정

--- src/techspec/TechspecService.java
+++ src/techspec/TechspecService.java
@@ -22,12 +22,13 @@ public class TechspecService {
     /**
      * (개발 중) 2. 스택 추가 (C)
      * @param currentUser 현재 로그인한 사용자
+     * @param techName [!] Controller에서 입력받은 기술 이름
      */
-    public void addTechspec(Member currentUser) {
+    public void addTechspec(Member currentUser,String techName) {
         System.out.println("\n---------- (개발 중) 스택 추가 ----------");
+        System.out.println(currentUser.getEmail() + "님에게 '" + techName + "' 스택을 추가합니다.");
 
         // (다음 로직)
-        // 1. (Controller에서 입력받은 기술 이름(예: "Java")을 파라미터로 받기)
         // 2. techspecRepository.findTechspecByName("Java") 호출 -> techspec_id 찾기
         // 3. techspecRepository.addMemberTechspec(currentUser.getId(), techspec_id) 호출
     }
@@ -35,12 +36,13 @@ public class TechspecService {
     /**
      * (개발 중) 3. 스택 삭제 (D)
      * @param currentUser 현재 로그인한 사용자
+     * @param techName [!] Controller에서 입력받은 기술 이름
      */
-    public void removeTechspec(Member currentUser) {
+    public void removeTechspec(Member currentUser, String techName) {
         System.out.println("\n---------- (개발 중) 스택 삭제 ----------");
+        System.out.println(currentUser.getEmail() + "님에게서 '" + techName + "' 스택을 삭제합니다.");
 
         // (다음 로직)
-        // 1. (Controller에서 입력받은 기술 이름(예: "Java")을 파라미터로 받기)
         // 2. techspecRepository.findTechspecByName("Java") 호출 -> techspec_id 찾기
         // 3. techspecRepository.deleteMemberTechspec(currentUser.getId(), techspec_id) 호출
     }
