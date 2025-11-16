bcdfce461d3d5841c1ae6c8779c7db0ee81354bc
chaechaekim <chaeeun7478@naver.com>
2025-11-17 02:28:43 +0900

    MemberMbtiService 추가

--- src/mbti/MemberMbtiService.java
+++ src/mbti/MemberMbtiService.java
@@ -1,4 +1,33 @@
-package src.mbti;
+package src.mbti; // [!] 새 패키지
 
+import java.util.List;
+import java.util.Map;
+
+/**
+ * [Member]의 MBTI 관련 비즈니스 로직을 담당
+ */
 public class MemberMbtiService {
+
+    private final MemberMbtiRepository mbtiRepository = new MemberMbtiRepository();
+
+    /**
+     * (R) Controller가 "질문지"를 요청
+     */
+    public List<MbtiDimension> getMbtiDimensions() {
+        return mbtiRepository.findAllMbtiDimensions();
+    }
+
+    /**
+     * (R) Controller가 "기존 답안지"를 요청
+     */
+    public Map<Long, String> getMbtiMapByMemberId(Long memberId) {
+        return mbtiRepository.findMbtiMapByMemberId(memberId);
+    }
+
+    /**
+     * (C/U) Controller가 "새 답안지" 저장을 요청
+     */
+    public boolean saveMyMbti(Long memberId, Map<Long, String> mbtiMap) {
+        return mbtiRepository.saveMemberMbti(memberId, mbtiMap);
+    }
 }
